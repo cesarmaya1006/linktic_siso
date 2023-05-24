046d5c414a8ee260fc27d424566adea396bf00ca
alejandro <alejandro.romero@linktic.co>
2023-05-24 17:01:59 -0500

    se corrigen conflictos

--- resources/views/extranet/plantilla.blade.php
+++ resources/views/extranet/plantilla.blade.php
@@ -5,7 +5,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/vnd.microsoft.icon" href="{{ asset('imagenes/sistema/icono_sistema.png') }}"
+    <link rel="icon" type="image/vnd.microsoft.icon" href="{{ asset('imagenes/sistema/logo.png') }}"
         sizes="64x64">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet"
